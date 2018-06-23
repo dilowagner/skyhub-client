743edd10390c6848af196ca10070aa11ff42a520
dilowagner <diegowagner4@gmail.com>
2018-06-22 21:31:20 -0300

    Enviando a Frete class

---
+++ src/Api/Freight.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace DW\SkyHub\Api;
+
+use DW\SkyHub\Route;
+use DW\SkyHub\Response;
+
+class Freight extends Api
+{
+    /**
+     * @var string
+     */
+    const FREIGHT_ROUTE = '/freights';
+
+    /**
+     * Recebe histórico de frete
+     * @return Response
+     * 
+     * GET /freights
+     */
+    public function list() : Response
+    {
+        return $this->client->get(new Route([self::FREIGHT_ROUTE]));
+    }
+}
