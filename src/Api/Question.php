3b250c8e9a2ca35ed2d689c88b85dd9f9d05f640
dilowagner <diegowagner4@gmail.com>
2018-06-25 22:35:35 -0300

    Enviando Question class

---
+++ src/Api/Question.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace DW\SkyHub\Api;
+
+use DW\SkyHub\Route;
+use DW\SkyHub\Response;
+
+class Question extends Api
+{
+    /**
+     * @var string
+     */
+    const QUESTION_ROUTE = '/questions';
+
+    /**
+     * Listar as Perguntas
+     * Filtros
+     *   Status
+     *   Plataforma
+     * 
+     * @param array $filter
+     * [
+     *   'status'   => 'UNANSWERED',
+     *   'platform' => 'Meli'
+     *   'cursor'   => 'fawsdert34534'
+     * ]
+     * 
+     * @return Response
+     * 
+     * GET /questions
+     */
+    public function list(array $filter = []) : Response
+    {
+        return $this->client->get(new Route([self::QUESTION_ROUTE]), $filter);
+    }
+
+    /**
+     * Visualizar os dados da Pergunta
+     * @param string $id
+     * @return Response
+     * 
+     * GET /questions/:id
+     */
+    public function view(string $id) : Response
+    {
+        return $this->client->get(new Route([self::QUESTION_ROUTE, $id]));
+    }
+
+    /**
+     * Responde uma Pergunta
+     * @param string $id
+     * @return Response
+     * 
+     * PUT /questions/:id/answer
+     */
+    public function answer(string $id) : Response
+    {
+        return $this->client->post(
+            new Route([self::QUESTION_ROUTE, $id, 'answer']), []
+        );
+    }
+
+    /**
+     * Remove uma Pergunta
+     * @param string $id
+     * @return Response
+     * 
+     * DELETE /questions/:id
+     */
+    public function remove(string $id) : Response
+    {
+        return $this->client->delete(new Route([self::QUESTION_ROUTE, $id]));
+    }
+}
