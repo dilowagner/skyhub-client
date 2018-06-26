85372d1dcc2f045c24ca099f788de2ae0a924a0c
dilowagner <diegowagner4@gmail.com>
2018-06-25 22:44:57 -0300

    Ajuste route Product

--- src/Api/Product.php
+++ src/Api/Product.php
@@ -11,7 +11,7 @@ class Product extends Api
     /**
      * @var string
      */
-    const PRODUCT_ROUTE = '/product';
+    const PRODUCT_ROUTE = 'product';
 
     /**
      * Lista os produtos cadastrados na plataforma
