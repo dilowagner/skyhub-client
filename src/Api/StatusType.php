f3533ae2ea792bbb01f94087793ff479d4c77c8d
dilowagner <diegowagner4@gmail.com>
2018-06-27 20:17:56 -0300

    Enviando StatusType class - para manipulacao de status de Pedidos

---
+++ src/Api/StatusType.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace DW\SkyHub\Api;
+
+use DW\SkyHub\Route;
+use DW\SkyHub\Response;
+
+class StatusType extends Api
+{
+    /**
+     * @var string
+     */
+    const STATUS_TYPE_ROUTE = 'status_types';
+
+    /**
+     * Recebe uma lista com os tipos de status disponíveis na atualização de pedidos
+     * @return Response
+     * 
+     * GET /status_types
+     */
+    public function list() : Response
+    {
+        return $this->client->get(new Route([self::STATUS_TYPE_ROUTE]));
+    }
+}
