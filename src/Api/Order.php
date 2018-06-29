48e45f1c93fb5646b630c5832f586e198cca61ce
dilowagner <diegowagner4@gmail.com>
2018-06-29 17:18:34 -0300

    Enviando Order class - Gerenciamento de Pedidos

---
+++ src/Api/Order.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace DW\SkyHub\Api;
+
+use DW\SkyHub\Route;
+use DW\SkyHub\Response;
+
+class Order extends Api
+{
+    /**
+     * @var string
+     */
+    const ORDER_ROUTE = 'orders';
+
+    /**
+     * Listar os pedidos que estão na SkyHub
+     * Filtros
+     *   Page
+     *   Per_Page
+     *   Sale_System
+     *   Statuses
+     * 
+     * @param array $filter
+     * @return Response
+     * 
+     * GET /orders
+     */
+    public function list(array $filter = []) : Response
+    {
+        return $this->client->get(new Route([self::ORDER_ROUTE]), $filter);
+    }
+}
